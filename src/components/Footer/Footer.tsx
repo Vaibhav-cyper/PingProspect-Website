37da132173a42bfef7f012eb95e17054e3fda1ba
Vaibhav Gupta <88025368+Vaibhav-cyper@users.noreply.github.com>
2025-02-27 22:34:58 +0530

    Update img path at Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -10,7 +10,7 @@ export default function Footer() {
           {/* Brand Section */}
           <div className="col-span-1 md:col-span-1">
             <div className="flex items-center space-x-2 mb-4">
-            <img src="Public\logo32x32.webp" alt="PingProspect" />
+            <img src="logo32x32.webp" alt="PingProspect" />
               <span className="text-xl font-bold text-gray-900">PingProspect</span>
             </div>
             <p className="text-gray-500 mb-4">
