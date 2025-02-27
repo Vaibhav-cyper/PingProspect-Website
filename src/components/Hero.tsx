f29942c02b9a50bd69e7cef53a85663cff2fc8d9
Vaibhav Gupta <gvaibhav2435@gmail.com>
2025-02-27 21:21:23 +0530

    fix img path

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -43,7 +43,7 @@ export default function Hero() {
       <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
         <img
           className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-          src="Public\hero.webp"
+          src="/hero.webp"
           alt="Desktop application interface"
         />
       </div>
