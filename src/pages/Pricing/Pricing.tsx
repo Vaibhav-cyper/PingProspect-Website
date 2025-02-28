492cbadc99fef63a4398dae9580ff023233988e1
Vaibhav Gupta <88025368+Vaibhav-cyper@users.noreply.github.com>
2025-02-28 13:09:37 +0530

    remove extra footer Pricing.tsx

--- src/pages/Pricing/Pricing.tsx
+++ src/pages/Pricing/Pricing.tsx
@@ -1,7 +1,7 @@
 import PricingHero from './PricingHero';
 import LaunchOffer from './LaunchOffer';
 import Support from '../../components/Support';
-import Footer from '../../components/Footer/Footer';
+
 
 export default function Pricing() {
   return (
@@ -9,7 +9,6 @@ export default function Pricing() {
       <PricingHero />
       <LaunchOffer />
       <Support />
-      <Footer />
     </div>
   );
 }
