41385626b97d81947f49d92e5a219ec31a9364a3
Vaibhav Gupta <88025368+Vaibhav-cyper@users.noreply.github.com>
2025-04-08 20:55:15 +0530

    Delete .next directory

--- .next/server/app/api/ValidateUser/route.js
+++
@@ -1,7 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/app/api/ValidateUser/route.js";
-const runtime = require("../../../chunks/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/node_modules_next_3ebedc43._.js");
-runtime.loadChunk("server/chunks/[root of the server]__d3881029._.js");
-runtime.getOrInstantiateRuntimeModule("[project]/.next-internal/server/app/api/ValidateUser/route/actions.js [app-rsc] (server actions loader, ecmascript)", CHUNK_PUBLIC_PATH);
-runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/ValidateUser/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH);
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/ValidateUser/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
