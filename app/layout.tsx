b81c68a8cff84f7600c0c24b7159e5fe8f6dc93f
Vaibhav Gupta <88025368+Vaibhav-cyper@users.noreply.github.com>
2025-05-01 16:49:24 +0530

    Add Google Tag in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,18 +35,21 @@ export default function RootLayout({children}: Readonly<{children: React.ReactNo
   return (
     <html lang="en">
       <head>
-        {/* Microsoft Clarity Script */}
-        <Script id="clarity" strategy="afterInteractive">
-          {`
-            (function(c,l,a,r,i,t,y){
-              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID}");
-          `}
-        </Script>
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer', "${process.env.NEXT_PUBLIC_TAG}");</script>
+        <!-- End Google Tag Manager -->
       </head>
       <body
+        
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src=`https://www.googletagmanager.com/ns.html?${process.env.NEXT_PUBLIC_TAG}`
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
+          
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Navbar/>
         {children}
